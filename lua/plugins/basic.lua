333de12ce06e6e071c5f99156d8abd34ccaba1d3
Bridger Brown <bridgerbrown@neuroflow.com>
2025-09-29 11:50:07 -0700

    lsp config update

--- lua/plugins/basic.lua
+++ lua/plugins/basic.lua
@@ -37,4 +37,41 @@ return {
     end,
   },
 
+   {
+    'goolord/alpha-nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      local alpha = require("alpha")
+      local dashboard = require("alpha.themes.dashboard")
+
+      -- Set header
+      dashboard.section.header.val = {
+        '███▄▄▄▄      ▄████████  ▄██████▄   ▄█    █▄   ▄█    ▄▄▄▄███▄▄▄▄   ',
+        '███▀▀▀██▄   ███    ███ ███    ███ ███    ███ ███  ▄██▀▀▀███▀▀▀██▄ ',
+        '███   ███   ███    █▀  ███    ███ ███    ███ ███▌ ███   ███   ███ ',
+        '███   ███  ▄███▄▄▄     ███    ███ ███    ███ ███▌ ███   ███   ███ ',
+        '███   ███ ▀▀███▀▀▀     ███    ███ ███    ███ ███▌ ███   ███   ███ ',
+        '███   ███   ███    █▄  ███    ███ ███    ███ ███  ███   ███   ███ ',
+        '███   ███   ███    ███ ███    ███ ███    ███ ███  ███   ███   ███ ',
+        ' ▀█   █▀    ██████████  ▀██████▀   ▀██████▀  █▀    ▀█   ███   █▀  ',
+      }
+
+      -- Set menu
+      dashboard.section.buttons.val = {
+          dashboard.button("e", "  > New file" , ":ene <BAR> startinsert <CR>"),
+          dashboard.button("f", "󰱼  > Find file", ":cd $HOME | Telescope find_files<CR>"),
+          dashboard.button("r", "  > Recent files"   , ":Telescope oldfiles<CR>"),
+          dashboard.button("z", '  > Suspend', ':suspend<CR>'),
+          dashboard.button("q", '  > Quit', '<cmd>qa<CR>'),
+      }
+
+      -- Send config to alpha
+      alpha.setup(dashboard.opts)
+
+      -- Disable folding on alpha buffer
+      vim.cmd([[
+          autocmd FileType alpha setlocal nofoldenable
+      ]])
+    end,
+  }
 }
