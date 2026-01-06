9a27602720cafd4cd04e68a01ffe04c936cec658
Bridger Brown <bridgerbrown@users.noreply.github.com>
2026-01-06 12:50:57 -0800

    git signs line blame and hotkey for commit message window

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,5 +1,4 @@
 return {
-
   -- Git related signs to the gutter
   {
     'lewis6991/gitsigns.nvim',
@@ -11,10 +10,24 @@ return {
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+        delay = 1000,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+        use_focus = true,
+      },
+      current_line_blame_formatter = '<author>, <author_time:%R>',
     },
+    config = function()
+      local gs = require("gitsigns")
+      gs.setup({})
+      vim.keymap.set("n", "<leader>lg", function() gs.blame_line() end)
+    end,
   },
 
   -- Git in Vim commands
   -- { "tpope/vim-fugitive", },
-
 }
